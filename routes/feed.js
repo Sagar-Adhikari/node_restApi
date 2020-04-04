6bf51d4107986a533483dfb147d1eb3c1f63351d
Sagar Adhikari <sagradhkr48@gmail.com>
2020-04-04 22:37:08 +0545

    get and post brief done

--- routes/feed.js
+++ routes/feed.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const router = express.Router();
 
 const feedController = require('../controllers/feed');
 
+const router = express.Router();
+
+// GET /feed/posts
 router.get('/posts', feedController.getPosts);
+
+// POST /feed/post
+router.post('/post', feedController.createPost);
 
 module.exports = router;
